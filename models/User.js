36626f057bcd87c9ae01fa5d7b5b1eb13d876f66
Nikita Shukla <nikishukla319@gmail.com>
2026-09-12 10:07:36 +0530

    fix: use separate SchemeSaathi users collection

--- models/User.js
+++ models/User.js
@@ -32,6 +32,6 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("SchemeSaathiUser", userSchema, "schemesaathi_users");
 
 module.exports = User;
